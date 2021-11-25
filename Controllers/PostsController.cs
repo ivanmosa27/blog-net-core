5b41ca558a7ff652be8cd7a16be9b9bfc7898623
ivan.moraga <ivan.moraga@CMDB-1186>
2021-11-25 13:38:50 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,122 @@
+﻿using blog_net_core.EF;
+using blog_net_core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog_net_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private BLOGContext _context;
+        public PostsController(BLOGContext context)
+        {
+            _context = context;
+        }
+
+        [Route("GetAll")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            var posts = _context.Posts.ToList();
+            return Ok(posts);
+        }
+
+        [Route("Get/{postId}")]
+        [HttpGet]
+        public IActionResult Get(int postId)
+        {
+            var post = _context.Posts.Find(postId);
+            if(post is null)
+            {
+                return BadRequest("No Data Was Found!");
+            }
+            return Ok(post);
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public IActionResult Create(PostModel model)
+        {
+
+            Posts post = new Posts();
+            post.PostName = model.postName;
+            post.PostDescription = model.postDescription;
+            post.CreatedAt = DateTime.Today;
+            _context.Add(post);
+            _context.SaveChanges();
+
+            return Ok(post);
+        }
+
+        [Route("Update")]
+        [HttpPut]
+        public IActionResult Update(PostModel model)
+        {
+            if (model.postId ==0)
+            {
+                return BadRequest("No ID Specified.");
+            }
+            var post = _context.Posts.Find(model.postId);
+
+            if(post is null)
+            {
+                return BadRequest("Bad ID Specified.");
+            }
+
+            post.PostName = model.postName;
+            post.PostDescription = model.postDescription;
+            post.UpdatedAt = DateTime.Today;
+            _context.Attach(post);
+            _context.SaveChanges();
+            
+            return Ok(post);
+
+        }
+        
+        [Route("DeleteBD/{postId}")]
+        [HttpDelete]
+        public IActionResult Delete(int postId)
+        {
+            var post = _context.Posts.Find(postId);
+            if (post is null)
+            {
+                return BadRequest("No Data Was Found!");
+            }
+            _context.Remove(post);
+            _context.SaveChanges();
+            
+            return Ok(post);
+        }
+
+        [Route("SoftDelete")]
+        [HttpPut]
+        public IActionResult SoftDelete(PostModel model)
+        {
+            if (model.postId == 0)
+            {
+                return BadRequest("No ID Specified.");
+            }
+            var post = _context.Posts.Find(model.postId);
+
+            if (post is null)
+            {
+                return BadRequest("Bad ID Specified.");
+            }
+
+            post.DeletedAt = DateTime.Today;
+            _context.Attach(post);
+            _context.SaveChanges();
+
+            return Ok(post);
+
+        }
+
+    }
+}
---
+++ EF/Posts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace blog_net_core.EF
+{
+    public partial class Posts
+    {
+        public int PostId { get; set; }
+        public string PostName { get; set; }
+        public string PostDescription { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
