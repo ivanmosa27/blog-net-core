9b186bcfc406a0134e980673c2c6b1406f5aa6eb
Moraga Salvador <ivan.moraga@pfsgroup.es>
2022-03-14 14:16:34 +0100

    feature/:proof CI

---
+++ Project/Modules/Posts/Dto/PaginationParamsDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace blog_net_core.Project.Modules.Posts.Dto
+{
+    public class PaginationParamsDto
+    {
+
+       public int PerPage {get;set;}
+       public int currentPage {get;set;}
+
+       public PaginationParamsDto()
+       {
+
+           this.PerPage = 1;
+           this.currentPage = 1;
+
+       }
+
+       public PaginationParamsDto(int PerPage, int currentPage)
+       {
+           this.PerPage = PerPage > 5 ? 5 : PerPage;
+           this.currentPage = currentPage < 1 ? 1 : currentPage;
+       }
+    }
+}
